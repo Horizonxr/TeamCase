e3ee73b0d962384273ed1c0ecc544c731af9f76b
瀚垠 <547446767@qq.com>
2021-10-30 10:21:20 +0800

    fix：grade1Compute.py

--- grade1Compute.py
+++ grade1Compute.py
@@ -4,13 +4,13 @@ from gradeCompute import gradeCompute
 sgn=['-','+']
 class grade1compute(gradeCompute):
     def generate():
-        sig = ['+', '-']
-        count = random.randint(2, 5)
+        sig = ['+', '-'] #加减符号
+        count = random.randint(2, 5)#2至5个数的相加减
         nums = []
         operators = []
         str1 = ''
-        for i in range(count):
-            nums.append(random.randint(0, 100))
+        for i in range(count):#循环加减
+            nums.append(random.randint(0, 100))#随机生成0-100之内的数
             nums[i] = round(nums[i], 2)
         for i in range(count - 1):
             operators.append(sig[random.randint(0, 1)])
@@ -21,7 +21,7 @@ class grade1compute(gradeCompute):
         print(str1)
         # print(nums)
         # print(operators)
-        ans = eval(str1)
+        ans = eval(str1) #用函数计算简化代码
         ans = round(ans, 2)
         print(ans)
 
