38a71c9493c9beaf433168cf0d5cfeeccefb1b86
tiankuanyu <1175611675@qq.com>
2021-10-30 10:30:49 +0800

    grade56Compute modify

--- grade56Compute.py
+++ grade56Compute.py
@@ -18,7 +18,7 @@ class gradeCompute
 
 class grade56Compute(gradeCompute)
     def __init__(self)
-        self.sig = ['+', '-', '', '']
+        self.sig = ['+', '-', '', '']#init the def
     def compute()
         sig = ['+', '-', '', '']
         count = random.randint(2, 5)
@@ -26,12 +26,12 @@ class grade56Compute(gradeCompute)
         operators = []
         str1 = ''
         for i in range(count)
-            nums.append(random.uniform(0, 10001))
+            nums.append(random.uniform(0, 10001))#生成小数
             nums[i] = round(nums[i], 2)
         for i in range(count - 1)
             operators.append(sig[random.randint(0, 3)])
         for i in range(count - 1)
-            str1 += str(nums[i])
+            str1 += str(nums[i])#字符串合并
             str1 += operators[i]
         str1 += str(nums[count - 1])
         print(str1)
